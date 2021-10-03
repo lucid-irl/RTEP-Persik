41ab20d56fc4aaebfd4a6b1e363ff96cc6301b65
lucid-irl <tranhuyhoang3103@gmail.com>
2021-10-03 13:41:55 +0700

    init signup component

--- persik-ui/src/app/app.module.ts
+++ persik-ui/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
