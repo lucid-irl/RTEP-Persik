67cb80b0b349fb180b964b96c937e6b35637bca1
lucid-irl <tranhuyhoang3103@gmail.com>
2021-10-03 15:27:40 +0700

    add css to Sign Up interface

--- persik-ui/src/app/app-routing.module.ts
+++ persik-ui/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'sign-up', component: SignUpComponent}
 ];
 
 @NgModule({
