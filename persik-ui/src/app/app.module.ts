5c2002b9e9bb544cbbf069a8f818b8da1ca593e5
truongaxin123 <truongaxin123@gmail.com>
2021-10-03 13:35:12 +0700

    init login component

--- persik-ui/src/app/app.module.ts
+++ persik-ui/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
