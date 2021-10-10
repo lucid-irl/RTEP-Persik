35bca34117c86cf11671ddb9e9beb87a93d451de
truongaxin123 <truongaxin123@gmail.com>
2021-10-10 16:00:43 +0700

    init super large article

---
+++ persik-ui/src/app/components/super-large-article/super-large-article.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-super-large-article',
+  templateUrl: './super-large-article.component.html',
+  styleUrls: ['./super-large-article.component.scss']
+})
+export class SuperLargeArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
