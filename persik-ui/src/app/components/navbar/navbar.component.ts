2c34f5855254f4601b11bbcc231dfffaf7a858d6
truongaxin123 <truongaxin123@gmail.com>
2021-10-17 09:06:45 +0700

    edit navbar

--- persik-ui/src/app/components/navbar/navbar.component.ts
+++ persik-ui/src/app/components/navbar/navbar.component.ts
@@ -23,6 +23,10 @@ export class NavbarComponent implements OnInit {
     {avatarUrl: "../../../assets/images/241095135_1827315980774811_8230954543138555389_n.jpg", notificationContent:"Content lorem nhuyrnasdasndsamasdadandw qweqwesdasdad", publicDate:"2020-01-01T00:00:00"},
     {avatarUrl: "../../../assets/images/241095135_1827315980774811_8230954543138555389_n.jpg", notificationContent:"Content lorem nhuyrnasdasndsamasdadandw qweqwesdasdad", publicDate:"2020-01-01T00:00:00"},
     {avatarUrl: "../../../assets/images/241095135_1827315980774811_8230954543138555389_n.jpg", notificationContent:"Content lorem nhuyrnasdasndsamasdadandw qweqwesdasdad", publicDate:"2020-01-01T00:00:00"},
+    {avatarUrl: "../../../assets/images/241095135_1827315980774811_8230954543138555389_n.jpg", notificationContent:"Content lorem nhuyrnasdasndsamasdadandw qweqwesdasdad", publicDate:"2020-01-01T00:00:00"},
+    {avatarUrl: "../../../assets/images/241095135_1827315980774811_8230954543138555389_n.jpg", notificationContent:"Content lorem nhuyrnasdasndsamasdadandw qweqwesdasdad", publicDate:"2020-01-01T00:00:00"},
+    {avatarUrl: "../../../assets/images/241095135_1827315980774811_8230954543138555389_n.jpg", notificationContent:"Content lorem nhuyrnasdasndsamasdadandw qweqwesdasdad", publicDate:"2020-01-01T00:00:00"},
+    {avatarUrl: "../../../assets/images/241095135_1827315980774811_8230954543138555389_n.jpg", notificationContent:"Content lorem nhuyrnasdasndsamasdadandw qweqwesdasdad", publicDate:"2020-01-01T00:00:00"},
   ]
 
   public notificationActive: NotificationActiveType = NotificationActiveType.None;
