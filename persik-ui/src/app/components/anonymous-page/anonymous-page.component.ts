27dbe2596121744edca08a78a31e642a7684190b
truongaxin123 <truongaxin123@gmail.com>
2021-10-17 09:06:00 +0700

    edit anonymous page

--- persik-ui/src/app/components/anonymous-page/anonymous-page.component.ts
+++ persik-ui/src/app/components/anonymous-page/anonymous-page.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnonymousPageComponent implements OnInit {
 
-  public anony_list = [1, 2, 3, 4, 5, 6];
+  public anony_list = [1, 2, 3, 4, 5, 6, 7];
 
   constructor() { }
 
