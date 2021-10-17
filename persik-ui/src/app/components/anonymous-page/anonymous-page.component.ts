3f1c9dfd92c176ea936d2af1bd92d85f6f294867
truongaxin123 <truongaxin123@gmail.com>
2021-10-17 13:00:14 +0700

    edit anonymous page

--- persik-ui/src/app/components/anonymous-page/anonymous-page.component.ts
+++ persik-ui/src/app/components/anonymous-page/anonymous-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Comment } from '../../models/Comment';
 
 
 @Component({
@@ -11,9 +11,83 @@ export class AnonymousPageComponent implements OnInit {
 
   public anony_list = [1, 2, 3, 4, 5, 6, 7];
 
+  public comments: Comment[] = [
+    {
+      id: 1,
+      publicDate: '15/11/2015',
+      content: 'lorem ipsum dolor sit amet lorem.',
+      upvote: 10,
+      downvote: 13,
+      parrentId: 0,
+      replyAmount: 0
+    },
+    {
+      id: 2,
+      publicDate: '15/11/2015',
+      content: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet',
+      upvote: 10,
+      downvote: 13,
+      parrentId: 0,
+      replyAmount: 0
+    },
+    {
+      id: 3,
+      publicDate: '15/11/2015',
+      content: 'lorem ipsum dolor sit amet',
+      upvote: 10,
+      downvote: 13,
+      parrentId: 0,
+      replyAmount: 0
+    },
+    {
+      id: 4,
+      publicDate: '15/11/2015',
+      content: 'lorem ipsum dolor sit amet',
+      upvote: 10,
+      downvote: 13,
+      parrentId: 0,
+      replyAmount: 0
+    },
+    {
+      id: 5,
+      publicDate: '15/11/2015',
+      content: 'lorem ipsum dolor sit amet',
+      upvote: 10,
+      downvote: 13,
+      parrentId: 0,
+      replyAmount: 0
+    },
+    {
+      id: 6,
+      publicDate: '15/11/2015',
+      content: 'lorem ipsum dolor sit amet',
+      upvote: 10,
+      downvote: 13,
+      parrentId: 0,
+      replyAmount: 0
+    },
+    {
+      id: 7,
+      publicDate: '15/11/2015',
+      content: 'lorem ipsum dolor sit amet',
+      upvote: 10,
+      downvote: 13,
+      parrentId: 0,
+      replyAmount: 0
+    },
+  ];
+
+  public isReplyActive: boolean = false;
+  public repliedCommentId: number = 0;
+  public articleTitle: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public handleReplyCommentEvent(comment: Comment): void {
+    this.isReplyActive = true;
+    this.repliedCommentId = comment.id;
+  }
 }
