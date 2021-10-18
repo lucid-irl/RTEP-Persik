fb12fbecc83fa385afa5df71ea43a97545aa6869
lucid-irl <tranhuyhoang3103@gmail.com>
2021-10-18 14:41:09 +0700

    edit article-item component

--- persik-ui/src/app/components/article-item/article-item.component.ts
+++ persik-ui/src/app/components/article-item/article-item.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
-enum VoteStatus {
-  Active ="active",
-  None = ""
-}
+import { Component, Input, OnInit } from '@angular/core';
+import { VoteStatus } from 'src/app/enums/VoteStatus';
+import { Article } from 'src/app/models/Article';
 
 @Component({
   selector: 'app-article-item',
@@ -12,29 +9,74 @@ enum VoteStatus {
 })
 export class ArticleItemComponent implements OnInit {
 
-  public votes: number = 0;
-  public isUpVoteActive: VoteStatus = VoteStatus.None;
-  public userId: string = "";
-  public userUrl: string = `http://localhost:4200/user/${this.userId}`;
+  @Input() Article!: Article;
+
+  public isShortContent: boolean = true;
+  public shortContent: string = '';
+  private LIMIT_CHARS: number = 100;
+
+  public isUpVoteActive:boolean = false;
+  public isDownVoteActive:boolean = false;
+  public voteStatus: VoteStatus = VoteStatus.None;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.shortContent = this.truncate(this.Article.content, this.LIMIT_CHARS);
   }
 
-  public upvote(): void {
-    if (this.isUpVoteActive === VoteStatus.None) {
-      this.isUpVoteActive = VoteStatus.Active;
-      this.votes++;
+  private truncate(content: string, n: number): string {
+    return (content.length > n) ? content.substr(0, n - 1) + '&hellip;' : content;
+  };
+
+  public showFullContent(): void {
+    this.isShortContent = false;
+  }
+
+  public showShortContent(): void {
+    this.isShortContent = true;
+  }
+
+  public upvote():void {
+    if (this.voteStatus == VoteStatus.None) {
+      this.voteStatus = VoteStatus.Upvote;
+      this.isUpVoteActive=true;
+      this.isDownVoteActive=false;
+      this.Article.upvote++;
+    }
+    else if (this.voteStatus == VoteStatus.Upvote) {
+      this.voteStatus = VoteStatus.None
+      this.isUpVoteActive = false;
+      this.isDownVoteActive = false;
+      this.Article.upvote--;
     }
     else {
-      this.isUpVoteActive = VoteStatus.None;
-      this.votes--;
+      this.voteStatus = VoteStatus.Upvote;
+      this.isUpVoteActive = true;
+      this.isDownVoteActive = false;
+      this.Article.downvote--;
+      this.Article.upvote++;
     }
   }
 
-  public downvote(): void {
-    
+  public downvote():void {
+    if (this.voteStatus == VoteStatus.None) {
+      this.voteStatus = VoteStatus.Downvote;
+      this.isDownVoteActive = true;
+      this.isUpVoteActive = false;
+      this.Article.downvote++;
+    }
+    else if (this.voteStatus == VoteStatus.Downvote) {
+      this.voteStatus = VoteStatus.None;
+      this.isDownVoteActive=false;
+      this.Article.downvote--;
+    }
+    else {
+      this.voteStatus = VoteStatus.Downvote;
+      this.isUpVoteActive = false;
+      this.isDownVoteActive = true;
+      this.Article.upvote--;
+      this.Article.downvote++;
+    }
   }
-
 }
