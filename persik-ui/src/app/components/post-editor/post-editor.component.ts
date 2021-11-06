c8482282c90dbcc6513a2bb7e9d41f90e89667d9
truongaxin123 <truongaxin123@gmail.com>
2021-11-06 09:13:31 +0700

    edit post editor

--- persik-ui/src/app/components/post-editor/post-editor.component.ts
+++ persik-ui/src/app/components/post-editor/post-editor.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ArticleState } from 'src/app/enums/ArticleState';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { ChangeEvent } from '@ckeditor/ckeditor5-angular/ckeditor.component';
+import { UploadImageServiceService } from 'src/app/services/upload-image-service.service';
 
 @Component({
   selector: 'app-post-editor',
@@ -7,16 +10,23 @@ import { ArticleState } from 'src/app/enums/ArticleState';
   styleUrls: ['./post-editor.component.scss']
 })
 export class PostEditorComponent implements OnInit {
-  @ViewChild('editor') editor: any;
-  public currentArticleState:ArticleState = ArticleState.Public;
+  public Editor = ClassicEditor;
 
-  constructor() { }
+  public model = {
+    editorData: '<p>Hello, world!</p>'
+  };
+
+  public title: string = '';
+  public currentArticleState: ArticleState = ArticleState.Public;
+
+  constructor(private uploadImageService: UploadImageServiceService) { }
 
   ngOnInit(): void {
+    
   }
 
-  public changeArticleState():void {
-    if(this.currentArticleState == ArticleState.Public){
+  public changeArticleState(): void {
+    if (this.currentArticleState == ArticleState.Public) {
       this.currentArticleState = ArticleState.Anonymous;
     }
     else {
@@ -24,4 +34,37 @@ export class PostEditorComponent implements OnInit {
     }
   }
 
+  public onChange({ editor }: ChangeEvent) {
+
+  }
+
+  public post() {
+    console.log(this.model.editorData);
+    console.log(this.title);
+    console.log(this.currentArticleState);
+  }
+
+  public onReady(editor: any) {
+    console.log(typeof (editor));
+    editor.plugins.get('FileRepository').createUploadAdapter = (loader: any) => {
+      return {
+        upload: () =>
+          new Promise((resolve, reject) => {
+            console.log(loader);
+            loader.file.then((file: any) => {
+              const formData = new FormData();
+              formData.append('upload', file);
+              // Call API upload hình và trả vào resolve url của image
+              this.uploadImageService.uploadTempImage(formData)
+                .subscribe(data => {
+                  resolve({
+                    default: data.url
+                  });
+                })
+            })
+          }),
+        abort: () => { console.log('Abort image upload.'); }
+      };
+    };
+  }
 }
