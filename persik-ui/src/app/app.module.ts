c79e993212ff9fbda85800682d9393f11011bb90
truongaxin123 <truongaxin123@gmail.com>
2021-11-06 09:15:25 +0700

    edit app modules

--- persik-ui/src/app/app.module.ts
+++ persik-ui/src/app/app.module.ts
@@ -15,7 +15,6 @@ import { NotificationItemComponent } from './components/notification-item/notifi
 import { AnonymousBoxComponent } from './components/anonymous-box/anonymous-box.component';
 import { AnonymousItemComponent } from './components/anonymous-item/anonymous-item.component';
 import { PostEditorComponent } from './components/post-editor/post-editor.component';
-import { QuillModule } from 'ngx-quill';
 import { AnonymousArticleComponent } from './components/anonymous-article/anonymous-article.component';
 import { AnonymousPageComponent } from './components/anonymous-page/anonymous-page.component';
 import { AnonymousCommentComponent } from './components/anonymous-comment/anonymous-comment.component';
@@ -25,23 +24,16 @@ import { PollArticleComponent } from './components/poll-article/poll-article.com
 import { AnonymousNavigationBarComponent } from './components/anonymous-navigation-bar/anonymous-navigation-bar.component';
 import { TagPageComponent } from './components/tag-page/tag-page.component';
 import { ArticleItemComponent } from './components/article-item/article-item.component';
-<<<<<<< HEAD
 import { SearchPageComponent } from './components/search-page/search-page.component';
 import { SearchUserComponent } from './components/search-user/search-user.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
 import { PollEditorComponent } from './components/poll-editor/poll-editor.component';
 import { PollEditorItemComponent } from './components/poll-editor-item/poll-editor-item.component';
-=======
-import { ChangePasswordComponent } from './change-password/change-password.component';
-<<<<<<< HEAD
-import { PollEditorComponent } from './components/poll-editor/poll-editor.component';
-import { PollEditorItemComponent } from './components/poll-editor-item/poll-editor-item.component';
-=======
->>>>>>> 4856ad9773dbf020577b78641d6dbe728207972b
->>>>>>> cae369e15483738a061941ff1656aa44ac8ece45
-
-
-
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ArticleHorizontalComponent } from './components/article-horizontal/article-horizontal.component';
 
 @NgModule({
   declarations: [
@@ -71,21 +63,15 @@ import { PollEditorItemComponent } from './components/poll-editor-item/poll-edit
     ChangePasswordComponent,
     PollEditorComponent,
     PollEditorItemComponent,
+    HomeComponent,
+    ArticleHorizontalComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    QuillModule.forRoot({
-      modules: {
-        toolbar: [
-          [{ header: [1, 2, false] }],
-          ['bold', 'italic', 'underline', 'strike'],
-          [{ 'align': [] }],
-          ['image', 'code-block']
-        ]
-      },
-      theme: 'snow'
-    })
+    CKEditorModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
