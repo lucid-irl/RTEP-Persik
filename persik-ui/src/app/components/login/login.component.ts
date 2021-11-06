07a2f72ea6856c2bda2aa0dde553987b2a9ce129
truongaxin123 <truongaxin123@gmail.com>
2021-11-06 09:16:57 +0700

    edit login component

--- persik-ui/src/app/components/login/login.component.ts
+++ persik-ui/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    username: null,
+    password: null
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  role: string = '';
+
+  constructor(private authService: AuthService, private tokenStorageService: TokenStorageService) { }
 
   ngOnInit(): void {
+    if (this.tokenStorageService.getToken()) {
+      this.isLoggedIn=true;
+      this.role = this.tokenStorageService.getUser().role;
+    }
   }
 }
