da0a7fbfe6ceb630040f9ec4d4d602831ba2eb45
truongaxin123 <truongaxin123@gmail.com>
2021-11-06 09:10:42 +0700

    add breakpoint tailwind css

--- persik-ui/tailwind.config.js
+++ persik-ui/tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': "1024px",
+      'xl': '1280px',
+      '2xl': '1536px',
+      'phone': '320px',
+      'laptop': '768px',
+    },
     fontFamily: {
       sans: ['Noto Sans', 'sans-serif'],
       serif: ['Open Sans', 'serif']
